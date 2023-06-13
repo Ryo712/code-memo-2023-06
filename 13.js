d1a2a814fccd6a0c9c969bfa3afdc08aa23f1565
大城亮 <hiltoc.katsu@gmail.com>
2023-06-13 21:12:50 +0900

    テンプレートリテラルの基本

---
+++ 13.js
@@ -0,0 +1,15 @@
+//テンプレートリテラル
+
+const score = 80;
+let test = '算数';
+const name = 'がずお';
+
+cosole.log(`${name}君の${test}の点数は${score}点です。`);
+//がずお君の算数のテストは80点です。
+
+const score = 80;
+  let test = '算数';
+  const name = 'がずお';
+  
+  cosole.log(name + '君の' + test + 'の点数は' + score + '点です。');
+  //テンプレートリテラルを使わない場合
